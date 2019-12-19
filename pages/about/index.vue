eead2d0cba2df8517b62a8b40032c05daa17cf99
takawiramundure <takawiramundure@gmail.com>
2019-12-18 23:49:20 -0500

    pages
    remove pages posts

--- pages/about/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section id="about-page">
-    <h1>Hi, that's us!</h1>
-    <p>We're a bunch of highly motivated and talented people, blogging about tech stuff.</p>
-  </section>
-</template>
